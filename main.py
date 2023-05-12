1f5adc78490efe790e235611e2459a2197f7822e
natihash <57513657+natihash@users.noreply.github.com>
2023-05-12 15:23:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,19 +21,19 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpeg", "jpg", "tif
 # model.load_state_dict(torch.load("unet64ppres_4chan_80", map_location=device))
 # model.eval()
 #
-# if uploaded_file is not None:
-#     image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
-#     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-#     n = stainNorm_Vahadane.Normalizer()
-#     hem = n.hematoxylin(image)
-#
+if uploaded_file is not None:
+    image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    n = stainNorm_Vahadane.Normalizer()
+    hem = n.hematoxylin(image)
+
 #     image = torch.from_numpy(image/255)
 #     image = image.permute(2, 0, 1)
-#
+
 #     hem = torch.from_numpy(hem/255)
 #     hem = hem.unsqueeze(0)
 #     image = torch.cat((image, hem), 0)
-#
+
 #     image = image.to(device, dtype=torch.float32)
 #     aa, bb = model(image.unsqueeze(0))
-#     st.image(image, caption="Uploaded Image")
+    st.image(hem, caption="Uploaded Image")
