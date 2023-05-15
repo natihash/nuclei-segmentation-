40fcb4f8046235f407d5e89e986feee3fab9e0c9
natihash <57513657+natihash@users.noreply.github.com>
2023-05-15 14:19:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,31 +10,25 @@ import gdown
 # import torch
 # import torch.nn as nn
 # import torchvision
-# from models import NestedUNet
-# import pysftp
-# my_Hostname = "pan.blockelite.cn"  
-# my_Username = "17780590451"  
-# my_Password = "R7mh9Mya"  
-# aa = pysftp.CnOpts()
-# aa.hostkeys = None   
-# sftp=pysftp.Connection(host = my_Hostname,port = 15021,username = my_Username,password = my_Password,cnopts=aa)
-# st.write("connected")
+from models import NestedUNet
 
-url="https://drive.google.com/uc?id=1OuVDKB1ElJ3DZyV-vSHULmRrUKPwIxxp"
-output_f = "my_model"
-gdown.download(url, output_f, quiet=False)
-st.write("done")
+
+# url="https://drive.google.com/uc?id=1OuVDKB1ElJ3DZyV-vSHULmRrUKPwIxxp"
+# output_f = "my_model"
+# gdown.download(url, output_f, quiet=False)
+# st.write("done")
 
 st.title("Image Uploader")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpeg", "jpg", "tif", "png"])
 
-# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# model = NestedUNet(num_classes=[1, 1], input_channels=4, deep_supervision=True)
-# model = nn.DataParallel(model)
-# model.to(device)
-# model.load_state_dict(torch.load("unet64ppres_4chan_80", map_location=device))
-# model.eval()
-#
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = NestedUNet(num_classes=[1, 1], input_channels=4, deep_supervision=True)
+model = nn.DataParallel(model)
+model.to(device)
+model.load_state_dict(torch.load("my_model", map_location=device))
+model.eval()
+st.write(device)
+
 if uploaded_file is not None:
     image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
